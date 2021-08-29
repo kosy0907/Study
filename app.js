9869ed75c8b953e67d1434c77232e62bd5176e88
kosy0907 <syko1234@naver.com>
2021-08-29 15:35:33 +0900

    new file

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3001);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use((req, res, next) => {
+    const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+  });
+  
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+app.listen(app.get('port'), () => {
+    console.log(`App listening at http://localhost:${app.get('port')}`)
+});
+
+module.exports = app;
