ba7c22246c681d26fa065354b40972fb146f4c7b
kosy0907 <syko1234@naver.com>
2021-12-16 16:08:31 +0900

    test

---
+++ props/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.name}</h1>
+        {this.props.content}
+      </header>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Subject name="REACT" content="FrameWork"></Subject>
+      <Subject name="WEB" content="World Wide Web"></Subject>
+    </div>
+  );
+}
+
+export default App;
---
+++ state/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello This is State Test
+    </div>
+  );
+}
+
+export default App;
