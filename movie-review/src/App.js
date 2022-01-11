4eef2dd81569086eb9b5e64393c2dacc15729d40
kosy0907 <syko1234@naver.com>
2022-01-11 21:00:25 +0900

    router 연결

--- movie-review/src/App.js
+++ movie-review/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route exact path='/' element={<Home />} />
+        {/* <Route path='/detail' element={<Detail /> } /> */}
+      </Routes>
+    </Router>
   );
 }
 
