4f249ea87facf9a6330faa732c8719052b4ba529
kosy0907 <syko1234@naver.com>
2022-01-14 14:05:39 +0900

    detail

--- movie-review/src/pages/Detail.js
+++ movie-review/src/pages/Detail.js
@@ -1,48 +1,68 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
-import StarRatings from 'react-star-ratings';
-import MovieInfo from "./sections/MovieInfo";
 
 export default function Detail() {
     const { movieId } = useParams();
 
     const movieUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=de638f81d320c814bb662ea24353aa8d`
     const creditUrl = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=de638f81d320c814bb662ea24353aa8d`
+    const imgBaseUrl = 'https://image.tmdb.org/t/p/w300/'
 
     const [info, setInfo] = useState();
     const [detail, setDetail] = useState([]);
 
+    const getInfo = () => {
+        fetch(movieUrl)
+            .then(response => response.json())
+            .then(response => {
+                setInfo(response)
+            })
+    }
     const getDetail = async () => {
         const movieDetail = await (await axios.get(creditUrl)).data.cast;
         setDetail([...movieDetail], []);
     }
 
     useEffect(() => {
+        getInfo();
         getDetail();
-
-        fetch(movieUrl)
-            .then(response => response.json())
-            .then(response => {
-                setInfo(response)
-            })
-
-
-        console.log(info);
         console.log(detail);
-    })
+    }, [])
 
     return (
         <div>
-            This is {movieId}'s page
+            {
+                info &&
+                <div>
+                    <div className="detail_poster">
+                        <img src={imgBaseUrl + info.poster_path} alt="movie-img" className="movie_img" />
+                    </div>
 
-            <div>
-                {/* movie Info */}
-                <MovieInfo
-                    movie={info}
-                    detail={detail}
-                />
-            </div>
+                    <div className="detail_info">
+                        <h3>{info.original_title}</h3>
+                        <ul>
+                            <li>{info.genres[0].name}</li>
+                            <li>{info.genres[1].name}</li>
+                        </ul>
+                    </div>
+
+                    <div className="detail_overview">
+                        {info.overview}
+                    </div>
+
+                    <div>
+                        <h3>Actor</h3>
+                        <ul>
+                            <li>{detail[0].name}: {detail[0].character}</li>
+                            <li>{detail[1].name}: {detail[1].character}</li>
+                            <li>{detail[2].name}: {detail[2].character}</li>
+                            <li>{detail[3].name}: {detail[3].character}</li>
+                            <li>{detail[4].name}: {detail[4].character}</li>
+                        </ul>
+                    </div>
+                </div>
+            }
 
             <div>
                 {/* actor Info */}
